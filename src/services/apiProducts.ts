be23ffa008861c66e14825289fa957a454c32b12
Thanasis Giannelos <athgiannelos@gmail.com>
2023-10-10 22:01:03 +0300

    fetch from dummyjson.com

--- src/services/apiProducts.ts
+++ src/services/apiProducts.ts
@@ -1,15 +1,20 @@
 import Product from "../features/products/product";
 
-const API_URL = "http://localhost:3004";
+// const API_URL = "http://localhost:3004";
+const API_URL = "https://dummyjson.com/products?limit=10";
 
 async function getProducts(): Promise<Product[]> {
-  const res = await fetch(`${API_URL}/products`);
+  // const res = await fetch(`${API_URL}/products?limit=20`);
+  const res = await fetch(API_URL);
 
   if (!res.ok) throw Error("Failed getting products");
 
-  const data = (await res.json()) as Product[];
+  // const data = (await res.json()) as Product[];
+  const data = (await res.json()) as {products: Product[], total: number, skip: number, limit: number};
 
-  return data;
+  const {products} = data;
+
+  return products as Product[];
 }
 
 async function getProduct(productId: string): Promise<Product> {
